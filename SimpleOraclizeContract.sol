b2660cb37a8b04ca7f0cf05a644c3774fbf1f2ea
bishesh16 <bishesh13@hotmail.com>
2018-06-09 22:52:15 +0100

    Create SimpleOraclizeContract.sol

---
+++ SimpleOraclizeContract.sol
@@ -0,0 +1,29 @@
+pragma solidity ^0.4.19;
+import "./Oraclize.sol";
+
+contract SimpleOraclizeContract is usingOraclize {
+
+    string public ETHXBT;
+    event LogConstructorInitiated(string nextStep);
+    event LogPriceUpdated(string price);
+    event LogNewOraclizeQuery(string description);
+
+    function ExampleContract() payable {
+        LogConstructorInitiated("Constructor was initiated. Call 'updatePrice()' to send the Oraclize Query.");
+    }
+
+    function __callback(bytes32 myid, string result) {
+        if (msg.sender != oraclize_cbAddress()) revert();
+        ETHXBT = result;
+        LogPriceUpdated(result);
+    }
+
+    function updatePrice() payable {
+        if (oraclize_getPrice("URL") > this.balance) {
+            LogNewOraclizeQuery("Oraclize query was NOT sent, please add some ETH to cover for the query fee");
+        } else {
+            LogNewOraclizeQuery("Oraclize query was sent, standing by for the answer..");
+            oraclize_query("URL", "json(https://api.kraken.com/0/public/Ticker?pair=ETHXBT).result.XETHXXBT.c.0");
+        }
+    }
+}
