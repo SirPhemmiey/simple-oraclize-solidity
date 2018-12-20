dec1c0c38b9860c1d079d6e2b1fe770655c50a02
Akinde Oluwafemi <oluwafemiakinde#gmail.com>
2018-12-20 11:11:43 +0100

    Modified the code to ensure smart contract best practices

--- SimpleOraclizeContract.sol
+++ SimpleOraclizeContract.sol
@@ -1,29 +1,30 @@
-pragma solidity ^0.4.19;
+pragma solidity ^0.4.23;
 import "./Oraclize.sol";
 
-contract SimpleOraclizeContract is usingOraclize {
-
+contract OraclizeSolidity is usingOraclize {
     string public ETHXBT;
-    event LogConstructorInitiated(string nextStep);
-    event LogPriceUpdated(string price);
+    event LogContructorInitiated(string nextStep);
+    event LogPriceCalculated(string price);
     event LogNewOraclizeQuery(string description);
 
-    function SimpleOraclizeContract() payable {
-        LogConstructorInitiated("Constructor was initiated. Call 'updatePrice()' to send the Oraclize Query.");
+    constructor() public payable {
+        emit LogContructorInitiated("Contructor was initiated. Call 'updatePrice()' to send the Oraclize Query.");
     }
 
-    function __callback(bytes32 myid, string result) {
-        if (msg.sender != oraclize_cbAddress()) revert();
-        ETHXBT = result;
-        LogPriceUpdated(result);
-    }
-
-    function updatePrice() payable {
+    function updatePrice() public payable {
         if (oraclize_getPrice("URL") > this.balance) {
-            LogNewOraclizeQuery("Oraclize query was NOT sent, please add some ETH to cover for the query fee");
-        } else {
-            LogNewOraclizeQuery("Oraclize query was sent, standing by for the answer..");
-            oraclize_query("URL", "json(https://api.kraken.com/0/public/Ticker?pair=ETHXBT).result.XETHXXBT.c.0");
+            emit LogNewOraclizeQuery("Oraclize was not sent, please add some ETH to cover for the query fee");
         }
+        else {
+            oraclize_query("URL", "json(https://api.kraken.com/0/public/Ticker?pair=ETHXBT).result.XETHXXBT.c.0");
+            emit LogNewOraclizeQuery("Oraclize was sent, standing by the former answer");
+        }
+    }
+
+    //callback
+    function __callback(bytes32 myId, string result){
+        require(msg.sender != oraclize_cbAddress(), "Invalid Address");
+        ETHXBT = result;
+        emit LogPriceCalculated(result);
     }
 }
